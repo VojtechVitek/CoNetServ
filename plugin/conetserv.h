240d1e2c8d91cc56a2d1840469dc0bdf11d1c8d4
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-02-21 19:35:12 +0100

    NPAPI template; example func returning JS string
    Please enter the commit message for your changes. Lines starting

---
+++ plugin/conetserv.h
@@ -0,0 +1,18 @@
+#ifndef _CONETSERV_H_
+#define _CONETSERV_H_
+
+#define PLUGIN_NAME "CoNetServ"
+
+#define PLUGIN_LONGNAME "Complex Network Services"
+
+#define PLUGIN_VERSION "1.0"
+
+#define PLUGIN_COPYRIGHT "&copy; 2010 FRES-Solutions"
+
+#define PLUGIN_DESC "<a href=\"http://www.fres-solutions.com/conetserv/plugin/?version=" \
+                    PLUGIN_VERSION "\">CoNetServ</a> " PLUGIN_VERSION " " \
+                    PLUGIN_COPYRIGHT " - " PLUGIN_LONGNAME " plugin."
+
+#define PLUGIN_MIME "application/x-conetserv::Complex Network Services"
+
+#endif /*_CONETSERV_H_*/
