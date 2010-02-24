16527aa30134792e89dc0d6ef6dc77560e02c2c4
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-02-24 21:20:44 +0100

    Parallel commands working in JS front-end

--- firefox/content/dialog.js
+++ firefox/content/dialog.js
@@ -1,40 +1,129 @@
 //alert(document.getElementById('conetservDialog').defaultButton);
 //alert(document.documentElement.getButton("accept").oncommand);
 
+/* CoNetServ NPAPI plugin to interact native code with */
 var conetserv = document.getElementById("conetserv");
-var result = document.getElementById("result");
-var interval;
+
+/* url to apply network tools for */
+var conetservUrl = document.getElementById("conetservUrl");
+
+/* console text-boxes */
+var pingConsole = document.getElementById("pingConsole");
+var tracerouteConsole = document.getElementById("tracerouteConsole");
+var whoisConsole = document.getElementById("whoisConsole");
+
+/* reading loop intervals */
+var pingInterval = false;
+var tracerouteInterval = false;
+var whoisInterval = false;
 
 function start() {
    document.getElementById('conetservStartButton').focus();
-   try {
-      conetserv.startPing("fres-solutions.com");
-      interval = window.setInterval("read()", 1000);
-   }
-   catch(e) {
-      alert(e);
-   }
-   result.value = "";
-}
 
-function read() {
-   try {
-      document.getElementById('conetservTabs').selectedPanel.firstChild.value = document.getElementById('conetservTabs').selectedPanel.firstChild.value + conetserv.readPing();
+   if (!pingInterval) {
+      try {
+         pingConsole.value = "";
+         if (conetserv.startPing(conetservUrl.value))
+            pingInterval = window.setInterval("readPing()", 500);
+         else
+            pingInterval = false;
+      }
+      catch(e) {
+         pingConsole.value = e;
+         pingInterval = false;
+      }
    }
-   catch(e) {
-      alert(e);
+
+   if (!tracerouteInterval) {
+      try {
+         tracerouteConsole.value = "";
+         if (conetserv.startTraceroute(conetservUrl.value))
+            tracerouteInterval = window.setInterval("readTraceroute()", 500);
+         else
+            tracerouteInterval = false;
+      }
+      catch(e) {
+         tracerouteConsole.value = e;
+         tracerouteInterval = false;
+      }
+   }
+
+   if (!whoisInterval) {
+      try {
+         whoisConsole.value = "";
+         if (conetserv.startWhois(conetservUrl.value))
+            whoisInterval = window.setInterval("readWhois()", 500);
+         else
+            whoisInterval = false;
+      }
+      catch(e) {
+         whoisConsole.value = e;
+         whoisInterval = false;
+      }
    }
 }
 
 function stop()
 {
+   if (pingInterval) {
+      try {
+         conetserv.stopPing();
+      }
+      catch(e) {
+         pingConsole.value = pingConsole.value + e;
+      }
+      window.clearInterval(pingInterval);
+      pingInterval = false;
+   }
+
+   if (tracerouteInterval) {
+      try {
+         conetserv.stopTraceroute();
+      }
+      catch(e) {
+         tracerouteConsole.value = tracerouteConsole.value + e;
+      }
+      window.clearInterval(tracerouteInterval);
+      tracerouteInterval = false;
+   }
+
+   if (whoisInterval) {
+      try {
+         conetserv.stopWhois();
+      }
+      catch(e) {
+         whoisConsole.value = whoisConsole.value + e;
+      }
+      window.clearInterval(whoisInterval);
+      whoisInterval = false;
+   }
+}
+
+function readPing() {
    try {
-       conetserv.stopPing();
+      pingConsole.value = pingConsole.value + conetserv.readPing();
    }
    catch(e) {
-      alert(e);
+      pingConsole.value = e;
    }
-   window.clearInterval(interval);
+}
+
+function readTraceroute() {
+   try {
+      tracerouteConsole.value = tracerouteConsole.value + conetserv.readTraceroute();
+   }
+   catch(e) {
+      tracerouteConsole.value = e;
+   }
+}
+
+function readWhois() {
+   try {
+      whoisConsole.value = whoisConsole.value + conetserv.readWhois();
+   }
+   catch(e) {
+      whoisConsole.value = e;
+   }
 }
 
 function focusOnRun(){
