9c50d03ed75c601507c570de3fe601a6a08dee7a
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-02-26 15:36:32 +0100

    jQuery based JS front-end for Google Chrome

---
+++ chrome/popup.js
@@ -0,0 +1,127 @@
+/* reading loop intervals */
+var pingInterval = -1;
+var tracerouteInterval = -1;
+var whoisInterval = -1;
+
+/* CoNetServ NPAPI plugin to interact native code with */
+var conetserv = document.getElementById("conetserv");
+
+/* url to apply network tools for */
+var conetservUrl = document.getElementById("conetservUrl");
+
+/* console text-boxes */
+var pingConsole = document.getElementById("pingConsole");
+var tracerouteConsole = document.getElementById("tracerouteConsole");
+var whoisConsole = document.getElementById("whoisConsole");
+
+/* reading loop intervals */
+var pingInterval = false;
+var tracerouteInterval = false;
+var whoisInterval = false;
+
+
+function readPing() {
+   try {
+     pingConsole.value = document.getElementById("pingConsole").value + document.getElementById("conetserv").readPing();
+   }
+   catch(e) {
+      document.getElementById("pingConsole").value = e;
+   }
+}
+
+function readTraceroute() {
+   try {
+      document.getElementById("tracerouteConsole").value = document.getElementById("tracerouteConsole").value + document.getElementById("conetserv").readTraceroute();
+   }
+   catch(e) {
+      document.getElementById("tracerouteConsole").value = e;
+   }
+}
+
+function readWhois() {
+   try {
+      document.getElementById("whoisConsole").value = document.getElementById("whoisConsole").value + document.getElementById("conetserv").readWhois();
+   }
+   catch(e) {
+      document.getElementById("whoisConsole").value = e;
+   }
+}
+
+function startCommands() {
+   if (pingInterval == -1) {
+      try {
+         document.getElementById("pingConsole").value = "";
+         if (document.getElementById("conetserv").startPing(document.getElementById("url").value))
+            pingInterval = window.setInterval("readPing()", 500);
+         else
+            pingInterval = -1;
+      }
+      catch(e) {
+         document.getElementById("pingConsole").value = e;
+         pingInterval = -1;
+      }
+   }
+
+   if (tracerouteInterval == -1) {
+      try {
+         document.getElementById("tracerouteConsole").value = "";
+         if (document.getElementById("conetserv").startTraceroute(document.getElementById("url").value))
+            tracerouteInterval = window.setInterval("readTraceroute()", 500);
+         else
+            tracerouteInterval = -1;
+      }
+      catch(e) {
+         document.getElementById("tracerouteConsole").value = e;
+         tracerouteInterval = -1;
+      }
+   }
+
+   if (whoisInterval == -1) {
+      try {
+         document.getElementById("whoisConsole").value = "";
+         if (document.getElementById("conetserv").startWhois(document.getElementById("url").value))
+            whoisInterval = window.setInterval("readWhois()", 500);
+         else
+            whoisInterval = -1;
+      }
+      catch(e) {
+         document.getElementById("whoisConsole").value = e;
+         whoisInterval = -1;
+      }
+   }
+}
+
+function stopCommands() {
+   if (pingInterval != -1) {
+      try {
+         document.getElementById("conetserv").stopPing();
+      }
+      catch(e) {
+         document.getElementById("pingConsole").value = document.getElementById("pingConsole").value + e;
+      }
+      window.clearInterval(pingInterval);
+      pingInterval = -1;
+   }
+
+   if (tracerouteInterval != -1) {
+      try {
+         document.getElementById("conetserv").stopTraceroute();
+      }
+      catch(e) {
+         document.getElementById("tracerouteConsole").value = document.getElementById("tracerouteConsole").value + e;
+      }
+      window.clearInterval(tracerouteInterval);
+      tracerouteInterval = -1;
+   }
+
+   if (whoisInterval != -1) {
+      try {
+         document.getElementById("conetserv").stopWhois();
+      }
+      catch(e) {
+         document.getElementById("whoisConsole").value = document.getElementById("whoisConsole").value + e;
+      }
+      window.clearInterval(whoisInterval);
+      whoisInterval = -1;
+   }
+}
