f897c89134a47f526653421d7b1d07fcf96f7b5a
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-06-02 02:05:01 +0200

    CMake: Clean-up in project variables & info & desc

--- plugin/win/resource.h
+++ plugin/win/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by conetserv.rc
+// Used by resource.rc
 //
 #define IDD_MAIN                        101
 #define IDC_BUTTON_GO                   1002
