198b8b26a356a9427a99d7d2053d72b6d759cd72
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-07-02 20:09:48 +0200

    Plugin: Update and comment API documentation

--- plugin/api/conetserv.h
+++ plugin/api/conetserv.h
@@ -1,42 +1,206 @@
-/**
+/*!
+ * \brief CoNetServ object
+ *
  * CoNetServ plugin should be loaded into (X)HTML document using object tag:
  * <object id="conetserv" type="application/x-conetserv" width="0" height="0">
  *   Plugin FAILED to load
  * </object>
+ *
+ * Getting JavaScript variable from object tag:
+ * var conetserv = document.getElementById("conetserv");
+ *
  */
-var conetserv = document.getElementById("conetserv");
+class conetserv {
 
-/**
- * Plugin version
- */
-conetserv.version;
+   /*!
+    * \brief Plugin version
+    */
+   string version = "2.0.0";
 
-/**
- * PING command
- */
-conetserv.ping;
+   /*!
+    * \brief PING command
+    */
+   class ping {
 
-/**
- * PING6 comand
- */
-conetserv.ping6;
+      /*!
+       * \brief Start ping command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
 
-/**
- * TRACEROUTE command
- */
-conetserv.traceroute;
+      /*!
+       * \brief Stop ping command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
 
-/**
- * TRACEROUTE6 command
- */
-conetserv.traceroute6;
+      /*!
+       * \breaf Read ping command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
 
-/**
- * WHOIS command
- */
-conetserv.whois;
+   };
 
-/**
- * NSLOOKUP command
- */
-conetserv.traceroute;
+   /*!
+    * \brief PING6 comand
+    */
+   class ping6 {
+
+      /*!
+       * \brief Start ping6 command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
+
+      /*!
+       * \brief Stop ping6 command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
+
+      /*!
+       * \breaf Read ping6 command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
+
+   };
+
+   /**
+    * \brief TRACEROUTE command
+    */
+   class traceroute {
+
+      /*!
+       * \brief Start traceroute command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
+
+      /*!
+       * \brief Stop traceroute command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
+
+      /*!
+       * \breaf Read traceroute command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
+
+   };
+
+   /**
+    * \brief TRACEROUTE6 command
+    */
+   class traceroute6 {
+
+      /*!
+       * \brief Start traceroute6 command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
+
+      /*!
+       * \brief Stop traceroute6 command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
+
+      /*!
+       * \breaf Read traceroute6 command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
+
+   };
+
+   /**
+    * \brief WHOIS command
+    */
+   class whois {
+
+      /*!
+       * \brief Start whois command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
+
+      /*!
+       * \brief Stop whois command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
+
+      /*!
+       * \breaf Read whois command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
+
+   };
+
+   /**
+    * \brief NSLOOKUP command
+    */
+   class nslookup {
+
+      /*!
+       * \brief Start nslookup command
+       * @return bool True if success, false otherwise.
+       */
+      bool start() {
+         /* Native code */
+      };
+
+      /*!
+       * \brief Stop nslookup command
+       * @return bool True if success, false otherwise.
+       */
+      bool stop() {
+         /* Native code */
+      };
+
+      /*!
+       * \breaf Read nslookup command output
+       * @return string Data output.
+       */
+      string read() {
+         /* Native code */
+      };
+
+   };
+
+};
