fa6f388afd9885fcd986c1fab2cf05686617c331
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-07-19 01:14:20 +0200

    CMake: FreeBSD compatibility

--- CMakePlugins/FindXULRunnerSDK.cmake
+++ CMakePlugins/FindXULRunnerSDK.cmake
@@ -3,6 +3,7 @@
 # XULRunnerSDK_FOUND ("YES" or not defined)
 # XULRunnerSDK_INCLUDE_DIR (path to find the headers)
 
+# If the path is not explicitly defined, try to find it manually
 if(NOT XULRunnerSDK_INCLUDE_DIR)
 
 	file(GLOB path1 "/usr/include/xulrunner-*")
@@ -15,8 +16,9 @@ if(NOT XULRunnerSDK_INCLUDE_DIR)
 
 endif()
 
+# Test header file and set final variable if found
 if(XULRunnerSDK_INCLUDE_DIR AND EXISTS "${XULRunnerSDK_INCLUDE_DIR}/npapi.h")
 	set(XULRunnerSDK_FOUND "YES")
 else()
-	message(FATAL_ERROR "Be sure to specify INCLUDE directory of XULRunnerSDK.")
+	message(FATAL_ERROR "Be sure to specify ABSOLUTE path of INCLUDE directory of XULRunnerSDK.")
 endif()
