4bf2998e9280133bb727e8036b1d760812a34f7a
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-08-23 22:26:33 +0200

    Plugin: Split platform-dependent process data

--- plugin/unix/process.h
+++ plugin/unix/process.h
@@ -2,19 +2,17 @@
 #ifndef _PLUGIN_PROCESS_H_
 #define _PLUGIN_PROCESS_H_
 
-/* FIXME: pid_t - see FIXME bellow */
-#include <unistd.h>
+#include <unistd.h> /*pid_t*/
 
 #include "npapi.h"
 #include "plugin.h"
 
-/** Plugin process - struct */
+/** UNIX: Plugin process - struct */
 typedef struct _process {
    NPObject        obj;
-//   NPP             instance;
-
-/* FIXME: Move pid_t and pipe to typedef under unix/ directory */
    bool            running;
+
+   /* Platform-dependent data: */
    pid_t           pid;
    int             pipe[2];
 } process;
