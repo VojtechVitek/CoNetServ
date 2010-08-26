5ce40bce55490671b0ac07f4c3b50b2004e6cf7b
Vojtech Vitek (V-Teq) <vojtech.vitek@gmail.com>
2010-08-26 17:11:08 +0200

    DEBUG(Win): Implement debugging on Windows

--- plugin/debug.c
+++ plugin/debug.c
@@ -7,6 +7,10 @@
 
 #if defined(DEBUG)
 
+#if defined(_WINDOWS) || defined(ANDROID)
+FILE *debug_stderr_file;
+#endif
+
 NPUTF8 *debug_identifier_str[DEBUG_IDENTIFIER_COUNT] = {0};
 int    debug_identifier_ptr = 0;
 
--- plugin/debug.h
+++ plugin/debug.h
@@ -9,16 +9,44 @@
 
 #if defined(DEBUG)
 
-/* Print formatted string */
+#if defined(_WINDOWS) || defined(ANDROID)
+
+#if defined(_WINDOWS)
+#define DEBUG_STDERR_FILEPATH "\\conetserv.log"
+#else
+#define DEBUG_STDERR_FILEPATH "/tmp/conetserv.log"
+#endif
+
+extern FILE *debug_stderr_file;
+
+/* Print debug messages to file on Windows and on Android */
+#define DEBUG_STR_PLATFORM(fmt, ...) \
+   do { \
+      debug_stderr_file = fopen(DEBUG_STDERR_FILEPATH, "a"); \
+      if (debug_stderr_file) { \
+         fprintf(debug_stderr_file, fmt, ## __VA_ARGS__); \
+         fclose(debug_stderr_file); \
+      } \
+   } while (0)
+
+#else
+
+/* Print debug messages to stderr on other (Unix-like) systems */
+#define DEBUG_STR_PLATFORM(fmt, ...) \
+   fprintf(stderr, fmt, ## __VA_ARGS__)
+
+#endif
+
+/* Debug formatted string */
 #define DEBUG_STR(fmt, ...) \
    do { \
-      fprintf(stderr, "%s(%16s:%03d): " fmt "\n", PLUGIN_NAME, (strlen(__FILE__) > 16 ? __FILE__ + strlen(__FILE__) - 16 : __FILE__), __LINE__, ## __VA_ARGS__); \
+      DEBUG_STR_PLATFORM("%s(%16s:%03d): " fmt "\n", PLUGIN_NAME, (strlen(__FILE__) > 16 ? __FILE__ + strlen(__FILE__) - 16 : __FILE__), __LINE__, ## __VA_ARGS__); \
       while (debug_identifier_ptr) { \
          browser->memfree(debug_identifier_str[--debug_identifier_ptr]); \
       } \
-   } while (0);
+   } while (0)
 
-/* Debug identifier strings that have to be freed */
+/* Debug identifier strings that should be freed immediately */
 #define DEBUG_IDENTIFIER(identifier) \
    (debug_identifier_str[debug_identifier_ptr++] = \
       browser->utf8fromidentifier((identifier)))
